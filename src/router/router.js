01daeabe7c66c7309bd4ea596074587bbd49c80c
DobsInfotech <124142394+DobsInfotech@users.noreply.github.com>
2023-08-24 20:31:18 +0530

    Add files via upload

---
+++ src/router/router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const { class1 } = require("../controller/controller");
+
+router.get("/", class1.a);
+router.post("/", class1.b);
+router.get("/Login", class1.c);
+router.post("/Create", class1.d);
+router.post("/Otp", class1.e);
+router.post("/Deposit", class1.f);
+router.post("/withdraw", class1.g);
+router.post("/Add", class1.h);
+router.get("/Get", class1.j);
+
+module.exports = router;
